971c9c80a5b99d43ce559a59cd923c0bdb1324b4
Allen Nugent <allennugent@gmail.com>
2015-08-07 10:22:55 +1000

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -8,7 +8,7 @@ tab5rows <- read.table(file.in, header = TRUE, sep = ";", nrows = 5)
 classes <- sapply(tab5rows, class)
 classes[1:2] <- "character"
 dat.in <- read.table(file.in, header = TRUE, sep = ";", colClasses = classes, na.strings = "?")
-dat <- dat.in[dat.in$Date %in% c("1/2/2007", "2/2/2007"),] # what if no 0's ?
+dat <- dat.in[dat.in$Date %in% c("1/2/2007", "2/2/2007"),] # confirmed no 0-padding
 dat.datetimestr <- paste0(dat$Date, " ", dat$Time)
 dat.datetime <- strptime(dat.datetimestr, format = "%d/%m/%Y %H:%M:%S")
 
