3a2a57da28f320482276783e374bbdebaa955c55
Allen Nugent <allennugent@gmail.com>
2015-08-07 10:20:37 +1000

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -8,7 +8,7 @@ tab5rows <- read.table(file.in, header = TRUE, sep = ";", nrows = 5)
 classes <- sapply(tab5rows, class)
 classes[1:2] <- "character"
 dat.in <- read.table(file.in, header = TRUE, sep = ";", colClasses = classes, na.strings = "?")
-dat <- dat.in[dat.in$Date %in% c("1/2/2007", "2/2/2007"),] # what if no 0's ?
+dat <- dat.in[dat.in$Date %in% c("1/2/2007", "2/2/2007"),] # confirmed no 0-padding
 dat.out <- dat$Global_active_power
 
 hist(dat.out, breaks = 12, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")#, ylab = "Frequency")
